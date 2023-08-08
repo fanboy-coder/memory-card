0abb47dbb01da61f5340209189c5bf64749ee0a5
Fanboy Coder <joel.monteiro.ferreira@gmail.com>
2023-08-08 16:51:33 +0100

    Finished the memory card project.

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,12 +1,19 @@
-import React, {useState} from "react";
+import PropTypes from "prop-types";
 
 export default function Scoreboard (props) {
 
 	return(
 		<>
 		<div>
-			<p>{props.scoretype} {props.score}</p>
+			<p>{props.scoreType} {props.score}</p>
 		</div>
 		</>
 	)
 }
+
+Scoreboard.propTypes = {
+	scoreType: PropTypes.string.isRequired, 
+	score: PropTypes.number.isRequired,     
+  };
+
+
